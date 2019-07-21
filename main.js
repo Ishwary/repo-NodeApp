6d1eb80fb4a05d04846824fb27aa9dbc7d7e695e
root <root@instance-2.us-central1-a.c.meta-episode-233208.internal>
2019-07-21 07:35:00 +0000

    Latest version

--- main.js
+++ main.js
@@ -3,15 +3,8 @@ var http = require('http');
 
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});  
-  response.write(html);
-  response.write(head);
-  response.write(body);
-  response.write("Hello Ishwar");
-  response.write(/body);
-  response.write(/head);
-  response.write(/html);
-  response.end(); 
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello NodeApp!! \n"); 
 });
 
 // listen on localhost:8000
