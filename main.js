543aa0f199dea3bc11c0b7d1099be7d3b05a3c88
root <root@instance-2.us-central1-a.c.meta-episode-233208.internal>
2019-07-21 06:16:17 +0000

    V0.4

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello NodeApp!! \n");
+  response.end("I am Onkar Mundalikp!! \n");
 });
 
 // listen on localhost:8000
